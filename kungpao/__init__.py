850bd228a9531adf56e867b1f83bdefa2611061f
dr.guangtou <dr.guangtou@gmail.com>
2015-12-03 01:33:51 +0900

    #1 Start to work

---
+++ kungpao/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                                unicode_literals)
+
+__version__ = "0.0.1"
+
+import os
+import re
+import subprocess
+import warnings
