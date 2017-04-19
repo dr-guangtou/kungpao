46151042bf2e7cfedacdd7d735c02daf38872bf8
dr.guangtou <dr.guangtou@gmail.com>
2017-04-18 18:13:50 -0700

    Placeholder for tests

---
+++ kungpao/test/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+__version__ = "0.0.1"
