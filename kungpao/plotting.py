8b301e2c5acc5dc428e851f4b1cec53f8ef5baef
dr-guangtou <dr.guangtou@gmail.com>
2017-09-08 17:32:33 -0700

    Basic updates

---
+++ kungpao/plotting.py
@@ -0,0 +1,8 @@
+"""
+Functions for plotting results of photometry.
+"""
+
+from __future__ import division, print_function
+
+import os
+import numpy as np
--- kungpao/utils.py
+++ kungpao/utils.py
@@ -1,7 +1,8 @@
 """
 Random utilities
 """
-from __future__ import division, print_function
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import os
 import numpy as np
@@ -28,6 +29,7 @@ def get_time_label():
 def check_random_state(seed):
     """
     Turn seed into a `numpy.random.RandomState` instance.
+
     Parameters
     ----------
     seed : `None`, int, list of ints, or `numpy.random.RandomState`
@@ -36,10 +38,12 @@ def check_random_state(seed):
         return a new `~numpy.random.RandomState` instance seeded with
         ``seed``.  If ``seed`` is already a `~numpy.random.RandomState`,
         return it.  Otherwise raise ``ValueError``.
+
     Returns
     -------
     random_state : `numpy.random.RandomState`
         RandomState object.
+
     Notes
     -----
     This routine is adapted from scikit-learn.  See
@@ -51,11 +55,51 @@ def check_random_state(seed):
         return np.random.mtrand._rand
     if isinstance(seed, (numbers.Integral, np.integer)):
         return np.random.RandomState(seed)
+    if isinstance(seed, (numbers.Real)):
+        # Song Huang: In case the seed is a float number, convert it to int
+        return np.random.RandomState(int(seed))
     if isinstance(seed, np.random.RandomState):
         return seed
-    if type(seed)==list:
-        if type(seed[0])==int:
+    if type(seed) == list:
+        if type(seed[0]) == int:
             return np.random.RandomState(seed)
 
     raise ValueError('{0!r} cannot be used to seed a numpy.random.RandomState'
                      ' instance'.format(seed))
+
+def random_cmap(ncolors=256, random_state=None):
+    """
+    Generate a matplotlib colormap consisting of random (muted) colors.
+    A random colormap is very useful for plotting segmentation images.
+
+    Parameters
+    ----------
+    ncolors : int, optional
+        The number of colors in the colormap.  The default is 256.
+    random_state : int or `~numpy.random.RandomState`, optional
+        The pseudo-random number generator state used for random
+        sampling.  Separate function calls with the same
+        ``random_state`` will generate the same colormap.
+
+    Returns
+    -------
+    cmap : `matplotlib.colors.Colormap`
+        The matplotlib colormap with random colors.
+
+    Notes
+    -----
+    Based on: https://github.com/astropy/photutils/blob/master/photutils/utils/colormaps.py
+    """
+
+    from matplotlib import colors
+
+    prng = check_random_state(random_state)
+
+    h = prng.uniform(low=0.0, high=1.0, size=ncolors)
+    s = prng.uniform(low=0.2, high=0.7, size=ncolors)
+    v = prng.uniform(low=0.5, high=1.0, size=ncolors)
+
+    hsv = np.dstack((h, s, v))
+    rgb = np.squeeze(colors.hsv_to_rgb(hsv))
+
+    return colors.ListedColormap(rgb)
