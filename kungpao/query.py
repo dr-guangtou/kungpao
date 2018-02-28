25d0ee63e1ffbb85725c3f4981b54fc7476369a5
dr-guangtou <dr.guangtou@gmail.com>
2018-02-28 14:42:04 -0800

    Move the image_gaia_stars functions here

---
+++ kungpao/query.py
@@ -0,0 +1,99 @@
+"""Database query."""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse as mpl_ellip
+
+from astroquery.gaia import Gaia
+
+from astropy import units as u
+from astropy.table import Column
+from astropy.units import Quantity
+from astropy.coordinates import SkyCoord
+
+from .display import display_single, ORG
+
+plt.rc('text', usetex=True)
+
+__all__ = ['image_gaia_stars']
+
+
+def image_gaia_stars(image, wcs, pixel=0.168, mask_a=694.7, mask_b=4.04,
+                     verbose=False, visual=False, size_buffer=1.4):
+    """Search for bright stars using GAIA catalog.
+
+    TODO:
+        Should be absorbed by the object for image later.
+
+    TODO:
+        Should have a version that just uses the local catalog.
+    """
+    # Central coordinate
+    ra_cen, dec_cen = wcs.wcs_pix2world(image.shape[0] / 2,
+                                        image.shape[1] / 2,
+                                        0)
+    img_cen_ra_dec = SkyCoord(
+        ra_cen, dec_cen, unit=('deg', 'deg'), frame='icrs')
+
+    # Width and height of the search box
+    img_search_x = Quantity(pixel * (image.shape)[0] * size_buffer, u.arcsec)
+    img_search_y = Quantity(pixel * (image.shape)[1] * size_buffer, u.arcsec)
+
+    # Search for stars
+    gaia_results = Gaia.query_object_async(
+        coordinate=img_cen_ra_dec,
+        width=img_search_x,
+        height=img_search_y,
+        verbose=verbose)
+
+    if len(gaia_results) > 0:
+        # Convert the (RA, Dec) of stars into pixel coordinate
+        ra_gaia = np.asarray(gaia_results['ra'])
+        dec_gaia = np.asarray(gaia_results['dec'])
+        x_gaia, y_gaia = wcs.wcs_world2pix(ra_gaia, dec_gaia, 0)
+
+        # Generate mask for each star
+        rmask_gaia_arcsec = mask_a * np.exp(
+            -gaia_results['phot_g_mean_mag'] / mask_b)
+
+        # Update the catalog
+        gaia_results.add_column(Column(data=x_gaia, name='x_pix'))
+        gaia_results.add_column(Column(data=y_gaia, name='y_pix'))
+        gaia_results.add_column(
+            Column(data=rmask_gaia_arcsec, name='rmask_arcsec'))
+
+        if visual:
+            fig = plt.figure(figsize=(8, 8))
+            ax1 = fig.add_subplot(111)
+
+            ax1 = display_single(image, ax=ax1)
+            # Plot an ellipse for each object
+            for star in gaia_results:
+                smask = mpl_ellip(
+                    xy=(star['x_pix'], star['y_pix']),
+                    width=(2.0 * star['rmask_arcsec'] / pixel),
+                    height=(2.0 * star['rmask_arcsec'] / pixel),
+                    angle=0.0)
+                smask.set_facecolor(ORG(0.2))
+                smask.set_edgecolor(ORG(1.0))
+                smask.set_alpha(0.3)
+                ax1.add_artist(smask)
+
+            # Show stars
+            ax1.scatter(
+                gaia_results['x_pix'],
+                gaia_results['y_pix'],
+                c=ORG(1.0),
+                s=100,
+                alpha=0.9,
+                marker='+')
+
+            ax1.set_xlim(0, image.shape[0])
+            ax1.set_ylim(0, image.shape[1])
+
+            return gaia_results, fig
+
+        return gaia_results
+
+    return None
