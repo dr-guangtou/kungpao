6c7b3c30be2b1be2478a1b62e0cf5a4d192097ca
dr-guangtou <dr.guangtou@gmail.com>
2018-08-23 00:34:49 -0700

    Add function img_rgb_figure()

--- kungpao/display.py
+++ kungpao/display.py
@@ -20,6 +20,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from matplotlib.ticker import AutoMinorLocator
 from matplotlib.colors import LinearSegmentedColormap
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
@@ -477,10 +478,10 @@ def diagnose_image_mask(img_mask, everything,
         color_bar=True)
 
     return fig
-    
+
 
 def science_cmap(cmap_name='vik', visual=False, list_maps=False):
-    """ Perceptually uniform color maps 
+    """ Perceptually uniform color maps
 
     Parameters
     ----------
@@ -488,7 +489,7 @@ def science_cmap(cmap_name='vik', visual=False, list_maps=False):
         Name of the colour map.
         Default: `vik`
     visual : boolen, optional
-        Whether to visualize the colour map or not. 
+        Whether to visualize the colour map or not.
         Default: False
     list_maps: boolen, optional
         If `True`, just list the names of available maps
@@ -501,7 +502,7 @@ def science_cmap(cmap_name='vik', visual=False, list_maps=False):
 
     Notes
     -----
-        Thanks Fabia Cramer for making these available. 
+        Thanks Fabia Cramer for making these available.
             http://www.fabiocrameri.ch/colourmaps.php
     """
     cmap_list = ['bamako', 'batlow', 'berlin', 'bilbao',
@@ -514,16 +515,106 @@ def science_cmap(cmap_name='vik', visual=False, list_maps=False):
     if cmap_name not in cmap_list:
         raise Exception("Wrong colour map name!")
     else:
-        cmap_pkl = resource_filename('kungpao', 
+        cmap_pkl = resource_filename('kungpao',
                                      '/cmap_data/%s.pkl' % cmap_name)
 
-        with open (cmap_pkl, 'rb') as cmap_file:
-            cmap = LinearSegmentedColormap.from_list(cmap_name, 
+        with open(cmap_pkl, 'rb') as cmap_file:
+            cmap = LinearSegmentedColormap.from_list(cmap_name,
                                                      pickle.load(cmap_file))
 
     if visual:
-        plt.imshow(np.linspace(0, 100, 256)[None, :], aspect='auto',      
-                   cmap=cmap)      
-        plt.show()      
-    
+        plt.imshow(np.linspace(0, 100, 256)[None, :], aspect='auto',
+                   cmap=cmap)
+        plt.show()
+
     return cmap
+
+
+def img_rgb_figure(image_r, image_g, image_b, stretch=0.5, Q=10,
+                   show=True, save=False, prefix='rgb', shrink=40,
+                   scale=0.168, physical=False, scalebar=None,
+                   scale_bar_y_offset=0.4, scale_bar_fontsize=15):
+    """Making RGB picture using the Lupton algorithm."""
+
+    assert image_r.shape == image_g.shape
+    assert image_r.shape == image_b.shape
+
+    img_h, img_w = image_r.shape
+
+    fig_h, fig_w = int(img_h / shrink), int(img_w / shrink)
+
+    image = make_lupton_rgb(image_r, image_g, image_b,
+                            stretch=stretch, Q=Q)
+
+    if show:
+        # --- Display --- #
+        fig = plt.figure(figsize=(fig_w, fig_h))
+        plt.subplots_adjust(left=0.002, bottom=0.002, right=0.998, top=0.998)
+        ax1 = fig.add_subplot(111)
+
+        # Adjust the frame
+        ax1.spines['top'].set_linewidth(3.0)
+        ax1.spines['top'].set_color('xkcd:silver')
+        ax1.spines['bottom'].set_linewidth(3.0)
+        ax1.spines['bottom'].set_color('xkcd:silver')
+        ax1.spines['left'].set_linewidth(3.0)
+        ax1.spines['left'].set_color('xkcd:silver')
+        ax1.spines['right'].set_linewidth(3.0)
+        ax1.spines['right'].set_color('xkcd:silver')
+
+        # Adjust the ticks
+        ## Remove tick labels
+        ax1.set_yticklabels([])
+        ax1.set_xticklabels([])
+
+        ## Set number of ticks
+        ax1.xaxis.set_major_locator(plt.MaxNLocator(8))
+        ax1.yaxis.set_major_locator(plt.MaxNLocator(8))
+        ax1.xaxis.set_minor_locator(AutoMinorLocator())
+        ax1.yaxis.set_minor_locator(AutoMinorLocator())
+
+        ## Adjust the tick legth, locations, width, and colors
+        ax1.tick_params(axis="both", direction="in", length=8,
+                        width=2.5, color='xkcd:grey',
+                        top=True, right=True, which='major')
+        ax1.tick_params(axis="both", direction="in", length=5,
+                        width=2.5, color='xkcd:grey',
+                        bottom=True, left=True,
+                        top=True, right=True, which='minor')
+
+        ax1.imshow(image, origin='lower')
+
+        # Scale bar
+        # TODO: the dealing of scale bar parameters is not perfect
+        if scalebar is not None:
+            # Pixel length
+            pixel_length = scalebar / scale
+
+            scale_bar_x_0 = int(img_w * 0.05)
+            scale_bar_x_1 = int(img_w * 0.05 + pixel_length)
+
+            scale_bar_y = int(img_h * 0.12)
+            scale_bar_text_x = (scale_bar_x_0 + scale_bar_x_1) / 2
+            scale_bar_text_y = (scale_bar_y * scale_bar_y_offset)
+
+            if physical:
+                scale_bar_text = r'$%d\ \mathrm{kpc}$' % int(scalebar)
+            else:
+                scale_bar_text = r'$%d^{\prime\prime}$' % int(scalebar)
+
+            ax1.plot(
+                [scale_bar_x_0, scale_bar_x_1], [scale_bar_y, scale_bar_y],
+                linewidth=2.0, c='w', alpha=1.0)
+            ax1.text(scale_bar_text_x, scale_bar_text_y,
+                     scale_bar_text, fontsize=scale_bar_fontsize,
+                     horizontalalignment='center', color='w')
+
+        if save:
+            fig.savefig(prefix + '.png', dpi=90)
+            plt.close()
+
+            return image
+        else:
+            return image, fig
+
+    return image
