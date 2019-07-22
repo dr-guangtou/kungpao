4445c9d9c7b31e490464dc213da45925017071ca
dr-guangtou <dr.guangtou@gmail.com>
2019-07-21 19:04:21 -0700

    Add a Pyraf bmodel wrapper

--- kungpao/galsbp/bmodel.py
+++ kungpao/galsbp/bmodel.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 """Building 2-D model based on the ellipse result."""
 
-__all__ = []
+import os
+import warnings
 
-def pyraf_bmodel(binary, parent, output=None, highar=False, verbose=False, interp='spline'):
+from astropy.io import fits
+
+__all__ = ['pyraf_bmodel']
+
+PYRAF_INTERP = ["nearest", "linear", "poly3", "spline"]
+
+def pyraf_bmodel(binary, parent, output=None, highar=False, verbose=False,
+                 interp='spline', backgr=0.0):
     """Wrapper of the `Pyraf` `bmodel` function to build 2-D model.
 
-    For details about the `bmodel` task, please see: 
+    For details about the `bmodel` task, please see:
         http://stsdas.stsci.edu/cgi-bin/gethelp.cgi?bmodel
 
     Parameters
@@ -23,7 +31,7 @@ def pyraf_bmodel(binary, parent, output=None, highar=False, verbose=False, inter
     verbose: boolen, optional
         Verbose mode. Default: False.
     interp: str, optional
-        The interpolation mode used by the `trebin` task. 
+        The interpolation mode used by the `trebin` task.
         Allowed values are: nearest | linear | poly3 | spline. Default: spline.
 
     Returns
@@ -32,7 +40,53 @@ def pyraf_bmodel(binary, parent, output=None, highar=False, verbose=False, inter
         2-D model image of the `ellipse` results.
 
     """
-    return 
+    # Initiate the isophote module in IRAF/STSDAS
+    try:
+        from pyraf import iraf
+        iraf.stsdas()
+        iraf.analysis()
+        iraf.isophote()
+    except ImportError:
+        raise Exception("# Unfortunately, you need to install pyraf first.")
+
+    # Make sure the files are available
+    if not os.path.isfile(binary):
+        raise FileNotFoundError(
+            "# Can not find the binary output file: {}".format(binary))
+    if not os.path.isfile(parent):
+        raise FileNotFoundError(
+            "# Can not find the input image: {}".format(parent))
+    
+    # Name of the output FITS file
+    if output is None:
+        output = os.path.splitext(binary)[0] + '_model.fits'
+
+    # Remove the output file if it exists
+    if os.path.isfile(output):
+        os.remove(output)
+
+    # Check the interpolation method
+    if interp not in PYRAF_INTERP:
+        raise Exception(
+            "# Wrong interpolation method! Choices are {}".format(PYRAF_INTERP))
+
+    # Higher-order harmonic modes
+    highar_str = "yes" if highar else "no"
+    verbose_str = "yes" if verbose else "no"
+
+    try:
+        iraf.bmodel(binary, output, parent=parent, interp=interp, highar=highar_str,
+                    backgr=backgr, verbose=verbose_str)
+    except Exception:
+        warnings.warn("# Something is wrong with the Bmodel task!")
+        return None
+
+    if not os.path.isfile(output):
+        raise FileNotFoundError("# Cannot find the output file: {}".format(output))
+    else:
+        model = fits.open(output)[0].data
+        return model
+
 
 def isophote_bmodel(isophote, binary, parent, output=None, highar=False, verbose=False,
                     interp='spline'):
